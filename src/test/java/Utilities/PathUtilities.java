0ff7a27f7fc67e9135a61ea47b0d3dbad643602c
andres.gomez <andres.gomez@reflexionhealth.comgit config --global user.email andres.gomez@reflexionhealth.comgit config --global user.email andres.gomez@reflexionhealth.com>
2017-10-05 13:17:56 -0700

    New path for extent-config

--- src/test/java/Utilities/PathUtilities.java
+++ src/test/java/Utilities/PathUtilities.java
@@ -10,7 +10,7 @@ public class PathUtilities {
     private static String chromeDriverPath = "/usr/local/share/chromedriver"; // "C:\\seleniumdriver\\chromedriver.exe";
     private static String excelPath = "C:\\Users\\User\\Documents\\NetBeansProjects\\CATest\\testData.xlsx";
     private static String extentRepPath = "/usr/local/share/reports/";
-    private static String extentConfPath = "/var/lib/jenkins/workspace/ca-maven-aws/src/test/resources/extent-config.xml";
+    private static String extentConfPath = "/var/lib/jenkins/workspace/ca-maven-aws-full/src/test/resources/extent-config.xml";
     private static String screenshotFFPath = "/usr/local/share/reports/firefox/";
     private static String screenshotCHPath = "/usr/local/share/reports/chrome/";
 
